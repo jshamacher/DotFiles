b4355173bb53bb90be54b87db4a74a3faf4d5f00
Josh Hamacher <hamacher@brutallogic.net>
2026-09-20 08:02:23 -0400

    Fix caps lock.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -230,7 +230,7 @@ hl.config({
         kb_layout  = "us",
         kb_variant = "",
         kb_model   = "",
-        kb_options = "",
+        kb_options = "ctrl:nocaps",
         kb_rules   = "",
 
         follow_mouse = 1,

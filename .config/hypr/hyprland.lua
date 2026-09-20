2ec3f300cb9210ac201ce698187563d1612ac8c4
Josh Hamacher <hamacher@brutallogic.net>
2026-09-20 08:02:36 -0400

    Launch some utilities on startup.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -48,11 +48,13 @@ local menu        = "fuzzel"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function ()
+hl.on("hyprland.start", function ()
 --   hl.exec_cmd(terminal)
 --   hl.exec_cmd("nm-applet")
 --   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+  hl.exec_cmd("mako")
+  hl.exec_cmd("waybar")
+end)
 
 
 -------------------------------
